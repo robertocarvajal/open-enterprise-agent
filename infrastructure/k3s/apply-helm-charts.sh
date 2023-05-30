2d61947644dd57c614a91a03c0f72e46cef65ad0
David Poltorak <david.poltorak@iohk.io>
2023-05-30 19:29:32 +0100

    feat: added install checks

---
+++ infrastructure/k3s/apply-helm-charts.sh
@@ -0,0 +1,198 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+Help() {
+	# Display Help
+	echo "Deploy Helm charts into k8s using argocd"
+	echo
+	echo "Syntax: apply-helm-charts.sh [-k/--kubectlexec KUBECTLCOMMAND|-h/--help]"
+	echo "options:"
+	echo "-k/--kubectlexec       Kubectl command to run to interact with k8s instance"
+	echo "-h/--help              Print this help text."
+	echo
+}
+
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+	case $1 in
+	-k | --kubectlexec)
+		KUBECTLCOMMAND="$2"
+		shift # past argument
+		shift # past value
+		;;
+	-h | --help)
+		Help
+		exit
+		;;
+	-* | --*)
+		echo "Unknown option $1"
+		Help
+		exit 1
+		;;
+	*)
+		POSITIONAL_ARGS+=("$1") # save positional arg
+		shift                   # past argument
+		;;
+	esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+if [[ -n $1 ]]; then
+	echo "Last line of file specified as non-opt/last argument:"
+	tail -1 "$1"
+fi
+
+if [ -z ${KUBECTLCOMMAND+x} ]; then
+	echo "-k/--kubectlexec is not set, exiting.";
+	exit;
+fi
+
+if ! command -v jq &>/dev/null; then
+	echo "jq could not be found"
+	exit
+fi
+
+echo "--------------------------------------"
+echo "deploying gittea via helm"
+echo "--------------------------------------"
+
+# Note - cannot apply to a directory as ordering of helm-charts matters
+
+# Local Git Serverr for managing state
+# kubectl apply --filename helm/gitea.yaml
+
+echo "--------------------------------------"
+echo "deploying argo-cd via helm"
+echo "--------------------------------------"
+
+# ArgoCD to manage all other components
+${KUBECTLCOMMAND} apply --filename helm/argocd.yaml
+
+echo "--------------------------------------"
+echo "waiting for argo-cd resources to be ready"
+echo "--------------------------------------"
+
+while ! ${KUBECTLCOMMAND} wait --namespace argocd --for=condition=Ready --timeout=-1s pod -l "app.kubernetes.io/name=argo-cd" -l "app.kubernetes.io/component=server"; do
+	echo "Resources do not exist yet and cannot use ${KUBECTLCOMMAND} wait command, sleeping 5 seconds"
+	sleep 5
+done
+
+ARGOCD_PASSWORD=$(${KUBECTLCOMMAND} get secret --namespace argocd argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode)
+
+echo "--------------------------------------"
+echo "argocd access credentials"
+echo "username: admin"
+echo "password: ${ARGOCD_PASSWORD}"
+echo "http://localhost:30443"
+echo "--------------------------------------"
+
+echo "--------------------------------------"
+echo "setting up ArgoCD git hub repository"
+echo "--------------------------------------"
+
+${KUBECTLCOMMAND} apply --filename helm/root-prism-platform-repo-secret.yaml
+
+echo "--------------------------------------"
+echo "checking ArgoCD github repo status"
+echo "--------------------------------------"
+
+argocd login localhost:30443 --username admin --password ${ARGOCD_PASSWORD} --insecure --plaintext
+
+REPO_STATUS=$(argocd repo get git@github.com:input-output-hk/atala-prism-helm-charts.git -o json | jq -r .connectionState.status)
+REPO_MESSAGE=$(argocd repo get git@github.com:input-output-hk/atala-prism-helm-charts.git -o json | jq -r .connectionState.message)
+if [[ "${REPO_STATUS}" == "Failed" ]]; then
+	echo "Repository did not correctly sync. Error message:"
+	echo "${REPO_MESSAGE}"
+	echo "Please check your private key in argocd.yaml"
+	echo "Deleting repository resource so that it can be recreated"
+	${KUBECTLCOMMAND} delete --filename helm/root-prism-platform-repo-secret.yaml
+	exit;
+fi
+
+echo "--------------------------------------"
+echo "deploying platform-root app-of-apps"
+echo "--------------------------------------"
+
+${KUBECTLCOMMAND} apply --filename helm/root-prism-platform-app.yaml
+
+echo "--------------------------------------"
+echo "waiting for platform-root applications"
+echo "--------------------------------------"
+
+echo "--------------------------------------"
+echo "waiting for external-secrets"
+echo "--------------------------------------"
+
+while ! ${KUBECTLCOMMAND} wait --namespace external-secrets --for=condition=Ready --timeout=-1s pod -l "app.kubernetes.io/name=external-secrets"; do
+	echo "Resources do not exist yet and cannot use ${KUBECTLCOMMAND} wait command, sleeping 5 seconds"
+	sleep 5
+done
+
+echo "--------------------------------------"
+echo "waiting for apisix"
+echo "--------------------------------------"
+
+while ! ${KUBECTLCOMMAND} wait --namespace ingress-apisix --for=condition=Ready --timeout=-1s pod -l "app.kubernetes.io/name=ingress-controller"; do
+	echo "Resources do not exist yet and cannot use ${KUBECTLCOMMAND} wait command, sleeping 5 seconds"
+	sleep 5
+done
+
+echo "--------------------------------------"
+echo "waiting for grafana"
+echo "--------------------------------------"
+
+while ! ${KUBECTLCOMMAND} wait --namespace monitoring --for=condition=Ready --timeout=-1s pod -l "app.kubernetes.io/name=grafana"; do
+	echo "Resources do not exist yet and cannot use ${KUBECTLCOMMAND} wait command, sleeping 5 seconds"
+	sleep 5
+done
+
+echo "--------------------------------------"
+echo "waiting for loki-stack"
+echo "--------------------------------------"
+
+while ! ${KUBECTLCOMMAND} wait --namespace monitoring --for=condition=Ready --timeout=-1s pod -l "name=loki-stack"; do
+	echo "Resources do not exist yet and cannot use ${KUBECTLCOMMAND} wait command, sleeping 5 seconds"
+	sleep 5
+done
+
+echo "--------------------------------------"
+echo "waiting for postgres-operator"
+echo "--------------------------------------"
+
+while ! ${KUBECTLCOMMAND} wait --namespace postgres-operator --for=condition=Ready --timeout=-1s pod -l "app.kubernetes.io/name=postgres-operator"; do
+	echo "Resources do not exist yet and cannot use ${KUBECTLCOMMAND} wait command, sleeping 5 seconds"
+	sleep 5
+done
+
+echo "--------------------------------------"
+echo "Setting up local routing"
+echo "--------------------------------------"
+
+${KUBECTLCOMMAND} apply --filename helm/routes/argocd-route.yaml
+${KUBECTLCOMMAND} apply --filename helm/routes/grafana-route.yaml
+${KUBECTLCOMMAND} apply --filename helm/routes/gitea-route.yaml
+
+echo "--------------------------------------"
+echo "argocd access credentials"
+echo "username: admin"
+echo "password: ${ARGOCD_PASSWORD}"
+echo "http://argocd.local-prism"
+echo "--------------------------------------"
+
+GRAFANA_PASSWORD=$(${KUBECTLCOMMAND} get secret --namespace monitoring kube-prometheus-stack-grafana -o jsonpath="{.data.admin-password}" | base64 --decode)
+
+echo "--------------------------------------"
+echo "grafana access credentials"
+echo "username: admin"
+echo "password: ${GRAFANA_PASSWORD}"
+echo "http://grafana.local-prism"
+echo "--------------------------------------"
+
+echo "--------------------------------------"
+echo "Set up complete"
+echo "--------------------------------------"
---
+++ infrastructure/k3s/install.sh
@@ -0,0 +1,128 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+# test if brew is installed
+if ! command -v brew &>/dev/null; then
+	echo "brew could not be found - please install brew (mac or linux)"
+	echo "https://brew.sh/"
+	exit
+fi
+
+install_common() {
+	# test if jq is installed via brew
+	if ! brew list jq &>/dev/null; then
+		echo "jq not installed, installing jq"
+		brew install jq
+	fi
+
+	# test if argocd is installed via brew
+	if ! brew list argocd &>/dev/null; then
+		echo "argocd not installed, installing argocd"
+		brew install argocd
+		exit
+	fi
+}
+
+install_linux() {
+	# test if k3s is installed
+	if ! command -v k3s &>/dev/null; then
+		echo "k3s not installed, installing k3s"
+		curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--disable=traefik" sh -
+	fi
+	KUBECTLCOMMAND="k3s kubectl"
+	# test if k3s is running
+	if ! systemctl is-active --quiet k3s; then
+		# start k3s
+		echo "k3s is not running - starting.."
+		sudo systemctl start k3s
+		until systemctl is-active --quiet k3s
+		do
+			echo "waiting for k3s to start"
+			sleep 5
+		done
+	fi
+}
+
+install_mac() {
+	# test if k3d is installed
+	if ! command -v k3d &>/dev/null; then
+		echo "k3d not installed, installing k3d"
+		brew install k3d@5.5.1
+		exit
+	fi
+	KUBECTLCOMMAND="kubectl"
+
+	# test if k3d has a cluster called local-cluster
+	if k3d cluster list | grep -q local-cluster; then
+		echo "local-cluster exists, continuing"
+	else
+		echo "local-cluster does not exist, creating local-cluster"
+		k3d kubeconfig merge local-cluster --kubeconfig-switch-context
+		k3d cluster create local-cluster --k3s-arg="--disable=traefik@server:0"
+	fi
+
+}
+
+install_common
+
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform
+	install_mac
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under GNU/Linux platform
+	install_linux
+else
+	echo "Unsupported OS, exiting"
+	exit 0;
+fi
+
+if [ -z ${KUBECTLCOMMAND+x} ]; then
+	echo "KUBECTLCOMMAND is not set, exiting.";
+	exit;
+fi
+
+echo "--------------------------------------"
+echo "wait for coredns startup"
+echo "--------------------------------------"
+
+while ! ${KUBECTLCOMMAND} wait --namespace kube-system --for=condition=Ready --timeout=-1s pod -l "k8s-app=kube-dns"; do
+	echo "Resources do not exist yet and cannot use kubectl wait command, sleeping 5 seconds"
+	sleep 5
+done
+
+echo "--------------------------------------"
+echo "modifying hosts file"
+echo "--------------------------------------"
+
+# check if host entry for grafana is in host file
+if grep -q "grafana.local-prism" /etc/hosts; then
+	echo "grafana.local-prism exists, continuing"
+else
+	echo "grafana.local-prism does not exist, adding grafana.local-prism to /etc/hosts"
+	sudo echo "127.0.0.1  grafana.local-prism" | sudo tee -a /etc/hosts
+fi
+
+# check if host entry for argocd is in host file
+if grep -q "argocd.local-prism" /etc/hosts; then
+	echo "argocd.local-prism exists, continuing"
+else
+	echo "argocd.local-prism does not exist, adding argocd.local-prism to /etc/hosts"
+	echo "127.0.0.1  argocd.local-prism" | sudo tee -a /etc/hosts
+fi
+
+# check if host entry for gitea is in host file
+if grep -q "gitea.local-prism" /etc/hosts; then
+	echo "gitea.local-prism exists, continuing"
+else
+	echo "gitea.local-prism does not exist, adding gitea.local-prism to /etc/hosts"
+	echo "127.0.0.1  gitea.local-prism" | sudo tee -a /etc/hosts
+fi
+
+# (jq -c . < ~/.docker/config.json | base64 -w 0)
+# jq -c . < ~/.docker/config.json | base64 -w 0 | xclip -selection clipboard
+# base64 prism-agent-1.1.0.tgz -w 0 | xclip -selection clipboard
+
+(${SCRIPT_DIR}/apply-helm-charts.sh -k "${KUBECTLCOMMAND}")
