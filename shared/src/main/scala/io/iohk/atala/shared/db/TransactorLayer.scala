72a6343dafc86e8105a02c8737444031ff0d313d
bvoiturier <benjamin.voiturier@iohk.io>
2023-07-31 14:13:18 +0200

    chore(shared): move connect DbConfig and TransactorLayer to shared module

--- shared/src/main/scala/io/iohk/atala/shared/db/TransactorLayer.scala
+++ shared/src/main/scala/io/iohk/atala/shared/db/TransactorLayer.scala
@@ -1,24 +1,20 @@
-package io.iohk.atala.connect.sql.repository
+package io.iohk.atala.shared.db
 
-import cats.effect.{Async, Resource}
-import doobie.util.transactor.Transactor
-import com.zaxxer.hikari.HikariConfig
-import doobie.util.ExecutionContexts
-import doobie.hikari.HikariTransactor
-import zio.interop.catz.*
-import zio.*
+import cats.effect.Async
+import cats.effect.kernel.{Resource, Sync}
 import cats.effect.std.Dispatcher
-
-case class DbConfig(
-    username: String,
-    password: String,
-    jdbcUrl: String,
-    awaitConnectionThreads: Int = 8
-)
+import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
+import doobie.util.ExecutionContexts
+import doobie.util.transactor.Transactor
+import io.iohk.atala.shared.models.{ContextRef, WalletAccessContext}
+import zio.interop.catz.*
+import zio.{Tag, TaskLayer, ZIO, ZLayer}
 
 object TransactorLayer {
 
-  def hikari[A[_]: Async: Dispatcher](config: DbConfig)(using tag: Tag[Transactor[A]]): TaskLayer[Transactor[A]] = {
+  def hikari[A[_]: Async: Dispatcher](config: DbConfig, contextRef: ThreadLocal[ContextRef[WalletAccessContext]])(using
+      tag: Tag[Transactor[A]]
+  ): TaskLayer[Transactor[A]] = {
     val transactorLayerZio = ZIO
       .attempt {
         // https://github.com/brettwooldridge/HikariCP/wiki/About-Pool-Sizing
@@ -35,13 +31,19 @@ object TransactorLayer {
           // Resource yielding a transactor configured with a bounded connect EC and an unbounded
           // transaction EC. Everything will be closed and shut down cleanly after use.
           ec <- ExecutionContexts.fixedThreadPool[A](config.awaitConnectionThreads) // our connect EC
-          xa <- HikariTransactor.fromHikariConfig[A](hikariConfig, ec)
-        } yield xa
+          ds <- createDataSourceResource(new WalletAwareDataSource(hikariConfig, contextRef))
+        } yield Transactor.fromDataSource[A](ds, ec)
 
         pool.toManaged.toLayer[Transactor[A]]
       }
 
     ZLayer.fromZIO(transactorLayerZio).flatten
+  }
+
+  private def createDataSourceResource[M[_]: Sync](factory: => HikariDataSource): Resource[M, HikariDataSource] = {
+    val alloc = Sync[M].delay(factory)
+    val free = (ds: HikariDataSource) => Sync[M].delay(ds.close())
+    Resource.make(alloc)(free)
   }
 
   private def makeHikariConfig(config: DbConfig): HikariConfig = {
